8bf9752ae25afc51fdfa0ba50028d6e8bf1ed123
amity94 <45635119+amity94@users.noreply.github.com>
2019-04-07 20:31:24 +0300

    Add files via upload

---
+++ 0704lesson/hw0704/hw0704/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw0704
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> company = new List<object>();
+
+            SQLiteConnection con = new SQLiteConnection($@"Data Source = C:\Users\HackerU\Desktop\0704hw.db; Version = 3");
+            con.Open();
+
+            using (SQLiteCommand cmd = new SQLiteCommand($"Select Employee.id, Employee.name as empName, Department.name as deptName from Employee" +
+                $" Join Department on Employee.Department_Id = Department.Id", con))
+            {
+                using (SQLiteDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read() == true)
+                    {
+                        var emp = new
+                        {
+                            ID = (Int64)reader["ID"],
+                            empName = (string)reader["empName"],
+                            deptName = (string)reader["deptName"]
+                        };
+                        company.Add(emp);
+                    }
+                }
+            }
+        }
+    }
+}
